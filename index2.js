404506641a44aae896e480170a681146b7389ee6
ReaderOnATree <67116075+ReaderOnATree@users.noreply.github.com>
2020-12-19 09:15:57 +0100

    zdarzenia_v2

--- index2.js
+++ index2.js
@@ -1,28 +1,39 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer(function (req, res) {
-    if (req.url == '/') {
-        fs.readFile('./titles.json', function (err, data) {
-            if (err) {
-                console.error(err);
-                res.end('titles.json error');
-            }
-            else {
-                let titles = JSON.parse(data.toString());
-                fs.readFile('./template.html', function (err, data) {
-                    if (err) {
-                        console.error(err);
-                        res.end('template.html error');
-                    }
-                    else {
-                        let tmpl = data.toString();
-                        let html = tmpl.replace('%', titles.join('</li><li>'));
-                        res.writeHead(200, {'Content-Type': 'text/html'});
-                        res.end(html);
-                    }
-                });
-            }
-        });
-    }
+let server = http.createServer(function(req, res) {
+    getTitiles(res);
 }).listen(8000, "127.0.0.1");
+
+function getTitiles(res) {
+    fs.readFile('./titles.json', function (err, data) {
+        if (err) {
+            hadError(err, res);
+        }
+        else {
+            getTemplate(JSON.parse(data.toString()), res);
+        }
+    });
+}
+
+function getTemplate(titiles, res) {
+    fs.readFile('./template.html', function (err, data) {
+        if (err) {
+            hadError(err, res);
+        }
+        else {
+            formatHtml(titles, data.toString(), res);
+        }
+    });
+}
+
+function formatHtml(titlles, tmpl, res) {
+    let html = tmpl.replace('%', titles.join('</li><li>'));
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+}
+
+function hadError(err, res) {
+    console.log(err);
+    res.end('Error');
+}
